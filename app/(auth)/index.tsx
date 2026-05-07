05145f74c75726cde488613a54d10d7974d9aa97
Manoel Carvalho <manoelmecat@gmail.com>
2026-05-07 16:35:37 -0300

    feat: login screen

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,5 +1,43 @@
-import { Text } from 'react-native';
-
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 export default function LoginScreen(){
-    return <Text>Qualquer cosia!</Text>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.txtLogin}>Login</Text>
+            <Text style={styles.txtWelcome}>Bem vindo(a) ao Paguedepois </Text>
+
+            <View style={styles.box}>
+                    <View>
+                        <Text>Email</Text>
+                        <TextInput />
+                    </View>
+
+            </View>
+        </View>
+    )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#184177',
+        paddingTop: 66,
+    },
+    txtLogin: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 64,
+        paddingLeft: 29,
+    },
+    txtWelcome:{
+        color: '#FFF',
+        fontSize: 20,
+        fontWeight: 400,
+        paddingLeft: 29,
+    },
+    box: {
+        backgroundColor: '#FFF',
+        width: '100%',
+        height: 632,
+        borderTopRightRadius: 24,
+        marginTop: 55
+    }
+});
